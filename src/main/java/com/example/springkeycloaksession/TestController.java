af3a3eaad8495616f3cc1ec11802ef150e80317f
asus <levietthang1997@gmail.com>
2023-06-17 15:48:40 +0700

    hoàn thành config spring boot với keycloak

--- src/main/java/com/example/springkeycloaksession/TestController.java
+++ src/main/java/com/example/springkeycloaksession/TestController.java
@@ -2,18 +2,48 @@ package com.example.springkeycloaksession;
 
 import org.keycloak.AuthorizationContext;
 import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.authorization.client.AuthzClient;
+import org.keycloak.authorization.client.ClientAuthorizationContext;
+import org.keycloak.authorization.client.representation.TokenIntrospectionResponse;
+import org.keycloak.representations.idm.authorization.AuthorizationRequest;
+import org.keycloak.representations.idm.authorization.AuthorizationResponse;
+import org.keycloak.representations.idm.authorization.Permission;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.PermitAll;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 @RequestMapping("/")
 public class TestController {
 
     @GetMapping("/")
-    public String test(HttpServletRequest httpServletRequest) {
-        return "OK";
+    public List<Permission> test(HttpServletRequest httpServletRequest) {
+        KeycloakSecurityContext keycloakSecurityContext = (KeycloakSecurityContext) httpServletRequest.getAttribute(KeycloakSecurityContext.class.getName());
+        AuthorizationContext authzContext = keycloakSecurityContext.getAuthorizationContext();
+        ClientAuthorizationContext clientContext = ClientAuthorizationContext.class.cast(authzContext);
+        AuthzClient authzClient = clientContext.getClient();
+        AuthorizationResponse authorizationResponse = authzClient.authorization().authorize(new AuthorizationRequest());
+
+        String rpt = authorizationResponse.getToken();
+        TokenIntrospectionResponse requestingPartyToken = authzClient.protection().introspectRequestingPartyToken(rpt);
+        return requestingPartyToken.getPermissions();
+    }
+
+    @GetMapping("/test")
+    @PermitAll
+    public List<Permission> test2(HttpServletRequest httpServletRequest) {
+        KeycloakSecurityContext keycloakSecurityContext = (KeycloakSecurityContext) httpServletRequest.getAttribute(KeycloakSecurityContext.class.getName());
+        AuthorizationContext authzContext = keycloakSecurityContext.getAuthorizationContext();
+        ClientAuthorizationContext clientContext = ClientAuthorizationContext.class.cast(authzContext);
+        AuthzClient authzClient = clientContext.getClient();
+        AuthorizationResponse authorizationResponse = authzClient.authorization().authorize(new AuthorizationRequest());
+
+        String rpt = authorizationResponse.getToken();
+        TokenIntrospectionResponse requestingPartyToken = authzClient.protection().introspectRequestingPartyToken(rpt);
+        return requestingPartyToken.getPermissions();
     }
 }
