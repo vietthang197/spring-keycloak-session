227019a1891ad15f040aed7050a1c7bdbdc4e909
thanglv <levietthang1997@gmail.com>
2023-06-17 10:42:12 +0700

    update

--- src/main/java/com/example/springkeycloaksession/SpringKeycloakSessionApplication.java
+++ src/main/java/com/example/springkeycloaksession/SpringKeycloakSessionApplication.java
@@ -2,10 +2,8 @@ package com.example.springkeycloaksession;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableWebSecurity
 public class SpringKeycloakSessionApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/springkeycloaksession/TestController.java
+++ src/main/java/com/example/springkeycloaksession/TestController.java
@@ -2,8 +2,6 @@ package com.example.springkeycloaksession;
 
 import org.keycloak.AuthorizationContext;
 import org.keycloak.KeycloakSecurityContext;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +14,6 @@ public class TestController {
 
     @GetMapping("/")
     public String test(HttpServletRequest httpServletRequest) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return "OK";
     }
 }
